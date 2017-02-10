45f59d606cdaf84d807d11c80ade42e6a00ab640
pmontu <pmontu@gmail.com>
2017-02-10 14:42:06 +0530

    redirect to hello world

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,7 +10,8 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    # url(r'^$', hello.views.index, name='index'),
+    url(r'^$', 'gettingstarted.views.home'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
