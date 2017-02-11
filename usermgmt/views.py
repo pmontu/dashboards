108bdedbe09038a5e6a78dde1085f1c5a3ee427a
pmontu <pmontu@gmail.com>
2017-02-11 07:23:48 +0530

    user already exists serializer error from perform_create git diff

--- usermgmt/views.py
+++ usermgmt/views.py
@@ -7,3 +7,11 @@ from .serializers import UserSerializer
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        username = serializer.validated_data["username"]
+        try:
+            if User.objects.get(username=username):
+                raise serializers.ValidationError("User already exists")
+        except User.DoesNotExist:
+            serializer.save()
